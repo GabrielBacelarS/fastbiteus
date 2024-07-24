c1113a05b850a1f5e44499099a4aae7750e13444
Gabriel-Bacelar <144174937+GabrielBacelarS@users.noreply.github.com>
2024-07-24 08:25:06 -0400

    first commit

---
+++ VersaoCliente/android/app/src/main/kotlin/com/sixamtech/efood_multivendor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fastbiteus_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ VersaoRestaurant/lib/util/dimensions.dart
@@ -0,0 +1,23 @@
+class Dimensions {
+  static const double fontSizeExtraSmall = 14.0;
+  static const double fontSizeSmall = 16.0;
+  static const double fontSizeDefault = 18.0;
+  static const double fontSizeLarge = 20.0;
+  static const double fontSizeExtraLarge = 22.0;
+  static const double fontSizeOverLarge = 28.0;
+
+  static const double paddingSizeExtraSmall = 5.0;
+  static const double paddingSizeSmall = 10.0;
+  static const double paddingSizeDefault = 15.0;
+  static const double paddingSizeLarge = 20.0;
+  static const double paddingSizeExtraLarge = 25.0;
+
+  static const double radiusSmall = 5.0;
+  static const double radiusDefault = 10.0;
+  static const double radiusLarge = 15.0;
+  static const double radiusExtraLarge = 20.0;
+
+  static const int messageInputLength = 250;
+  static const double webMaxWidth = 1170;
+
+}
